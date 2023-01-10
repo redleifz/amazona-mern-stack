6c1ae988a69eb113bdd5027652de9bdac46da8b0
Jongjate Choomjairug <jongjate.ch@gmail.com>
2023-01-10 23:26:45 +0700

    dev-fix-proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_PROXY_HOST,
+      target:"https://amazona-backend.onrender.com",
       changeOrigin: true,
     })
   );
