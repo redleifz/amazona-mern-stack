8bf579cbec66daef00ce5f4e0a6516379f84d085
Jongjate Choomjairug <jongjate.ch@gmail.com>
2023-01-16 23:26:35 +0700

    50. List Users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,6 +31,7 @@ import AdminRoute from "./screens/AdminRoute";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
 import OrderListScreen from "./screens/OrderListScreen";
+import UserListScreen from "./screens/UserListScreen";
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -194,6 +195,15 @@ function App() {
                 element={
                   <AdminRoute>
                     <OrderListScreen />
+                  </AdminRoute>
+                }
+              ></Route>
+
+              <Route
+                path="/admin/users"
+                element={
+                  <AdminRoute>
+                    <UserListScreen />
                   </AdminRoute>
                 }
               ></Route>
